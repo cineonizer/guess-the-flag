b2c510d1eecfd6dc80da9c3cb637b248a89d7e87
cineonizer <cineonizer@gmail.com>
2022-04-12 18:39:04 -0700

    added flag assets and also set up the interface using storyboard interface builder

--- Guess the Flag/ViewController.swift
+++ Guess the Flag/ViewController.swift
@@ -8,8 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet var button1: UIButton!
+  @IBOutlet var button2: UIButton!
+  @IBOutlet var button3: UIButton!
+  var countries = [
+    "estonia",
+    "france",
+    "germany",
+    "ireland",
+    "italy",
+    "monaco",
+    "nigeria",
+    "poland",
+    "russia",
+    "spain",
+    "uk",
+    "us",
+  ]
+  var score = 0
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+  }
+
+  func displayRandomFlags() {
+    button1.setImage(UIImage(named: "estonia"), for: .normal)
   }
 }
